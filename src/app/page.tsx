26598e1436d9739a42df2fb8d68b9c26a4bb4cb4
Saeed Khan <skybypk@gmail.com>
2024-11-18 02:23:29 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+  <div>
+    Hello World, GIAIC Quarter 2 my first class for Next Js Teacher Mr.Ali Aftab Sheikh & Muhammad Bilal, 2024.
+  </div>
+  );
+}
